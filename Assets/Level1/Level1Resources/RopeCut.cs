d5ca41618d0cee1fb1c182a63c2f537be3b5ad51
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-04-07 22:22:33 -0400

    new feature: rope with timer

--- Assets/Level1/Level1Resources/RopeCut.cs
+++ Assets/Level1/Level1Resources/RopeCut.cs
@@ -4,11 +4,49 @@ using UnityEngine;
 
 public class RopeCut : MonoBehaviour
 {
+    public GameObject markerPrefab;
+    public float duration = 0.05f;
+    public float fadeTime = 0.05f;
+    public float ropeCutDelay = 30f;
+    private GameObject redRope;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the red rope
+        GameObject rope = GameObject.FindGameObjectWithTag("RedRope");
+        redRope = rope;
+
+        // Cut the red rope after a delay
+        if (redRope != null)
+        {
+            InvokeRepeating("CutRope", ropeCutDelay, 1f);
+        }
+    }
+
+    void CutRope()
+    {
+        // Find a random link in the red rope to cut
+        int linkCount = redRope.transform.childCount;
+        int linkIndex = Random.Range(0, linkCount);
+
+        // Destroy the link and its joint
+        GameObject link = redRope.transform.GetChild(linkIndex).gameObject;
+        Destroy(link.GetComponent<HingeJoint2D>());
+        Destroy(link);
+
+    }
+
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
+            Vector2 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject marker = Instantiate(markerPrefab, clickPosition, Quaternion.identity);
+            StartCoroutine(FadeMarker(marker));
+
             //Spawning ray at point of mouse position, ray is async pointer
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if(hit.collider != null)
@@ -19,5 +57,35 @@ public class RopeCut : MonoBehaviour
                 }
             }
         }
+    }
+
+    private IEnumerator FadeMarker(GameObject marker)
+    {
+        SpriteRenderer renderer = marker.GetComponent<SpriteRenderer>();
+        float timer = 0f;
+
+        // Fade in the marker
+        while (timer < fadeTime)
+        {
+            float alpha = Mathf.Lerp(0f, 0.2f, timer / fadeTime);
+            renderer.color = new Color(1f, 0f, 0f, alpha);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Wait for the duration
+        yield return new WaitForSecondsRealtime(duration - 2f * fadeTime);
+
+        // Fade out the marker
+        timer = 0f;
+        while (timer < fadeTime)
+        {
+            float alpha = Mathf.Lerp(0.2f, 0f, timer / fadeTime);
+            renderer.color = new Color(1f, 0f, 0f, alpha);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(marker);
     }
 }
