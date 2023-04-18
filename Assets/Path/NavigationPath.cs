a6e452771e708c172d322ea2aad24f8a50e263d7
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2023-04-18 01:20:59 -0400

    level 2 -> 3 transition

--- Assets/Path/NavigationPath.cs
+++ Assets/Path/NavigationPath.cs
@@ -17,6 +17,9 @@ public class NavigationPath : MonoBehaviour
     public Vector3 waypoint4;
 
     public GameObject waypoint1GameObject;
+    public GameObject waypoint2GameObject;
+    public GameObject waypoint3GameObject;
+    public GameObject waypoint4GameObject;
 
 
     private int currentLevelIndex = 1;
@@ -61,8 +64,18 @@ public class NavigationPath : MonoBehaviour
     {
         isTransitioning = true;
         targetPosition = GetLevelPosition(levelIndex);
+
         waypoint1GameObject = GameObject.FindWithTag("LevelPath 1");
         waypoint1 = waypoint1GameObject.transform.position;
+
+        waypoint2GameObject = GameObject.FindWithTag("LevelPath 2");
+        waypoint2 = waypoint2GameObject.transform.position;
+
+        waypoint3GameObject = GameObject.FindWithTag("LevelPath 3");
+        waypoint3 = waypoint3GameObject.transform.position;
+
+        waypoint4GameObject = GameObject.FindWithTag("LevelPath 4");
+        waypoint4 = waypoint4GameObject.transform.position;
 
         Vector3 startPosition = transform.position;
         float elapsedTime = 0.0f;
@@ -90,14 +103,55 @@ public class NavigationPath : MonoBehaviour
                 }
                 elapsedTime += Time.deltaTime;
             }
+            else if (currentLevelIndex == 3 && levelIndex == 3) // from level 2 to 3
+            {
+                // Move towards waypoint2 first
+                Vector3 waypoint2Position = waypoint2;
+                Vector3 waypoint3Position = waypoint3;
+                Vector3 waypoint4Position = waypoint4;
+                float distanceToWaypoint2 = Vector3.Distance(startPosition, waypoint2Position);
+                float distanceToWaypoint3 = Vector3.Distance(waypoint2Position, waypoint3);
+                float distanceToWaypoint4 = Vector3.Distance(waypoint3, waypoint4);
+                float distanceToTarget = Vector3.Distance(waypoint4, targetPosition);
+                float currentLerpTime = elapsedTime / transitionDuration;
+                float lerpTimeToWaypoint2 = distanceToWaypoint2 / (distanceToWaypoint2 + distanceToWaypoint3 + distanceToWaypoint4 + distanceToTarget);
+                float lerpTimeToWaypoint3 = distanceToWaypoint3 / (distanceToWaypoint2 + distanceToWaypoint3 + distanceToWaypoint4 + distanceToTarget);
+                float lerpTimeToWaypoint4 = distanceToWaypoint4 / (distanceToWaypoint2 + distanceToWaypoint3 + distanceToWaypoint4 + distanceToTarget);
+                float lerpTimeToTarget = 1f - lerpTimeToWaypoint2 - lerpTimeToWaypoint3 - lerpTimeToWaypoint4;
+
+                if (currentLerpTime < lerpTimeToWaypoint2)
+                {
+                    // Move towards waypoint2
+                    transform.position = Vector3.Lerp(startPosition, waypoint2Position, currentLerpTime / lerpTimeToWaypoint2);
+                }
+                else if (currentLerpTime < lerpTimeToWaypoint2 + lerpTimeToWaypoint3)
+                {
+                    // Move towards waypoint3
+                    transform.position = Vector3.Lerp(waypoint2Position, waypoint3, (currentLerpTime - lerpTimeToWaypoint2) / lerpTimeToWaypoint3);
+                }
+                else if (currentLerpTime < lerpTimeToWaypoint2 + lerpTimeToWaypoint3 + lerpTimeToWaypoint4)
+                {
+                    // Move towards waypoint4
+                    transform.position = Vector3.Lerp(waypoint3, waypoint4, (currentLerpTime - lerpTimeToWaypoint2 - lerpTimeToWaypoint3) / lerpTimeToWaypoint4);
+                }
+                else
+                {
+                    // Move towards targetPosition
+                    transform.position = Vector3.Lerp(waypoint4, targetPosition, (currentLerpTime - lerpTimeToWaypoint2 - lerpTimeToWaypoint3 - lerpTimeToWaypoint4) / lerpTimeToTarget);
+                }
+                elapsedTime += Time.deltaTime;
+            }
             else
             {
                 // Move towards targetPosition in a straight line
                 transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / transitionDuration);
                 elapsedTime += Time.deltaTime;
+
             }
 
             yield return null;
+
+
         }
 
 
