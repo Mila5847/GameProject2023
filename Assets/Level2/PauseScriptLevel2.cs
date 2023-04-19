668b17aa8806de7e9bf83972d992cd20c23af201
Saimirz <106036032+Saimirz@users.noreply.github.com>
2023-04-19 01:54:30 -0400

    level2
    added a pause menu

---
+++ Assets/Level2/PauseScriptLevel2.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScriptLevel2 : MonoBehaviour
+{
+    [SerializeField]
+    bool IsPaused = false;
+
+    [SerializeField]
+    GameObject PauseCanvas;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                ResumeMe();
+            }
+            else
+            {
+                PauseMe();
+            }
+
+        }
+
+    }
+
+    public void PauseMe()
+    {
+
+        Time.timeScale = 0;
+        IsPaused = true;
+        PauseCanvas.SetActive(true);
+
+
+    }
+
+    public void ResumeMe()
+    {
+
+        Time.timeScale = 1;
+        IsPaused = false;
+        PauseCanvas.SetActive(false);
+
+    }
+}
