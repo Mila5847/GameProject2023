63ab0abd56d56401b7df2937a6d93bccff11a06b
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-04-20 10:57:08 -0400

    trampoline bounce paddle

--- Assets/Level2/Level2Resources/TrampolineBounce.cs
+++ Assets/Level2/Level2Resources/TrampolineBounce.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrampolineBounce : MonoBehaviour
@@ -8,32 +9,19 @@ public class TrampolineBounce : MonoBehaviour
     public GameObject target;
     public Rigidbody2D rb;
 
-    private bool collided = false;
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("TrampolineDown"))
-        {
-            collided = true;
-            
-        }
-    }
-
-    private void FixedUpdate()
-    {
-       /* if (collided)
-        {
-            rb.MovePosition(Vector3.MoveTowards(transform.position, target.transform.position, 2 * Time.deltaTime));
-             Calculate the direction to the target position
-            Vector3 direction = targetPosition.position - objectToMove.position;
-
-            // Move towards the target position using Lerp
-            objectToMove.position = Vector3.Lerp(objectToMove.position, targetPosition.position, speed * Time.fixedDeltaTime);
-        }*/
-    }
+     private void OnTriggerEnter2D(Collider2D collision)
+     {
+         if (collision.gameObject.CompareTag("TrampolineDown"))
+         {
+             Rigidbody2D rb = GetComponent<Rigidbody2D>();
+             rb.constraints = RigidbodyConstraints2D.FreezeAll;
+             rb.transform.SetParent(collision.gameObject.transform);
+         }
+     }
 }
