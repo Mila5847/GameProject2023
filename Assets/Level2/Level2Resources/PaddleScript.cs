af498a47cdabefab24a48cb8b5c1aff455190ad3
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-04-23 14:54:57 -0400

    paddle fixed

---
+++ Assets/Level2/Level2Resources/PaddleScript.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+    // Reference to the meat object
+    private GameObject meat;
+
+    // Flag to check if the meat is stuck to the paddle
+    private bool meatStuck = false;
+
+    void Start()
+    {
+        // Find the meat object using its tag
+        meat = GameObject.FindGameObjectWithTag("Meat");
+    }
+
+    void Update()
+    {
+     
+        // If the meat is stuck to the paddle, move it along with the paddle
+        if (meatStuck)
+        {
+            meat.transform.position = transform.position + new Vector3(0, 0.50f, 0);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("In collision");
+        if (collision.gameObject.CompareTag("Meat") && !meatStuck)
+        {
+
+            // Make the meat a child of the paddle at the correct relative position
+            collision.transform.SetParent(transform);
+
+            // Set the meatStuck flag to true
+            meatStuck = true;
+        }
+    }
+}
--- Assets/Level2/Level2Resources/TrampolineBounce.cs
+++ Assets/Level2/Level2Resources/TrampolineBounce.cs
@@ -6,22 +6,60 @@ using UnityEngine;
 
 public class TrampolineBounce : MonoBehaviour
 {
-    public GameObject target;
-    public Rigidbody2D rb;
+    // Reference to the ball object
+    private GameObject ball;
+
+    // Flag to check if the ball is stuck to the paddle
+    private bool ballStuck = false;
+
+    // Speed at which the paddle moves
+    public float speed = 5f;
+
+    // Release force for the ball when it is released
+    public float releaseForce = 10f;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
+        // Find the ball object using its tag
+        ball = GameObject.FindGameObjectWithTag("Meat");
     }
 
-     private void OnTriggerEnter2D(Collider2D collision)
-     {
-         if (collision.gameObject.CompareTag("TrampolineDown"))
-         {
-             Rigidbody2D rb = GetComponent<Rigidbody2D>();
-             rb.constraints = RigidbodyConstraints2D.FreezeAll;
-             rb.transform.SetParent(collision.gameObject.transform);
-         }
-     }
+    void Update()
+    {
+        // Move the paddle horizontally
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+        // If the ball is stuck to the paddle, move it along with the paddle
+        if (ballStuck)
+        {
+            ball.transform.position = transform.position + new Vector3(0, 0.75f, 0);
+
+            // If the player presses the space key, release the ball
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ballStuck = false;
+                ball.GetComponent<Rigidbody2D>().isKinematic = false;
+                ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, releaseForce), ForceMode2D.Impulse);
+                ball.transform.parent = null;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("In Collision");
+        if (other.gameObject.CompareTag("TrampolineDown"))
+        {
+            Debug.Log("In Collision with trampoline");
+            // Make the ball a child of the paddle
+            other.gameObject.transform.parent = transform;
+
+            // Disable the ball's rigidbody
+            other.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+
+            // Set the ballStuck flag to true
+            ballStuck = true;
+        }
+    }
 }
