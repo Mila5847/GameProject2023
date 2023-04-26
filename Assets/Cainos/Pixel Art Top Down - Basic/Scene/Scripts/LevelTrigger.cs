dca1ef658b7896b3fd65c68cf4119dcb39f41535
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2023-04-25 20:31:39 -0400

    level trigger fixed

--- Assets/Cainos/Pixel Art Top Down - Basic/Scene/Scripts/LevelTrigger.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Scene/Scripts/LevelTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class LevelTrigger : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,6 @@ public class LevelTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private Dictionary<string, string> levelSceneMap = new Dictionary<string, string>()
@@ -24,13 +24,37 @@ public class LevelTrigger : MonoBehaviour
         { "level3", "Level3" }
     };
 
-    void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("in collison");
+        if (gameObject.CompareTag("level1"))
+        {
+            Debug.Log("level 1");
+            //SceneManager.LoadScene("Level1");
+                
+        }
+        if (gameObject.CompareTag("level2"))
+        {
+            Debug.Log("level 2");
+            //SceneManager.LoadScene("Level1");
+
+        }
+        if (gameObject.CompareTag("level3"))
+        {
+            Debug.Log("level 3");
+            //SceneManager.LoadScene("Level1");
+
+        }
+    }
+
+    /*void OnColliderEnter(Collider other)
+    {
+        Debug.Log("triggereddddd");
         string sceneName;
         if (levelSceneMap.TryGetValue(other.tag, out sceneName))
         {
             SceneManager.LoadScene(sceneName);
             Debug.Log("triggered");
         }
-    }
+    }*/
 }
