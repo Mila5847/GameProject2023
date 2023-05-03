424a30a36a51c76f1df2992336792ef574d0f6a8
KarinaSofia <104159293+KarinaSofia@users.noreply.github.com>
2023-05-03 11:01:51 -0400

    Animations fixed

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,40 +12,49 @@ public class AnimationController : MonoBehaviour
     public float maxX;
     public float maxY;
 
+    private Rigidbody2D rb2d;
+
+
+    private Vector3 lastPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var direction = transform.position - lastPosition;
+        var localDirection = transform.InverseTransformDirection(direction);
+        lastPosition = transform.position;
+
         animator.speed = 1;
-        float val_x = Input.GetAxis("Horizontal");
-        float val_y = Input.GetAxis("Vertical");
+        float val_x = localDirection.x;
+        float val_y = localDirection.y;
+        float abs_val_x = Math.Abs(val_x);
+        float abs_val_y = Math.Abs(val_y);
 
-        if (val_y < 0)//then set direction to 2
-        {
-            animator.SetInteger("Direction", (int)MoveDirectionPlayer.DOWN);
-        }
 
-        else if (val_y > 0) //then set direction to 1
-        {
-            animator.SetInteger("Direction", (int)MoveDirectionPlayer.UP);
-        }
 
-        else if (val_x < 0) //then set direction to 3
+        if (val_x < 0 && abs_val_y < abs_val_x) //then set direction to 3
         {
             animator.SetInteger("Direction", (int)MoveDirectionPlayer.LEFT);
         }
-
-        else if (val_x > 0) //then set direction to 4
+        else if (val_x > 0 && abs_val_y < abs_val_x) //then set direction to 4
         {
             animator.SetInteger("Direction", (int)MoveDirectionPlayer.RIGHT);
         }
-
+        else if (val_y < 0 && abs_val_y > abs_val_x)//then set direction to 2
+        {
+            animator.SetInteger("Direction", (int)MoveDirectionPlayer.DOWN);
+        }
+        else if (val_y > 0 && abs_val_y > abs_val_x) //then set direction to 1
+        {
+            animator.SetInteger("Direction", (int)MoveDirectionPlayer.UP);
+        }
         else
         {
 
