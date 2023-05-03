d5b9e58e0d0f7aef176d897f9f7aefeb8d94ff69
DenisaNicoletaH <denisanhategan@gmail.com>
2023-05-03 03:33:13 -0400

    PauseMemoryCardGame3done, need to add to all others

---
+++ Assets/MemoryGameScene/Scripts/PauseCardGame.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PauseCardGame : MonoBehaviour
+{
+    [SerializeField]
+    bool isPaused = false;
+
+    [SerializeField]
+    GameObject PauseCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+
+                ResumeMe();
+            }
+            else
+            {
+
+
+                PauseMe();
+            }
+
+        }
+    }
+
+
+    public void PauseMe()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        PauseCanvas.SetActive(true);
+    }
+
+    public void ResumeMe()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        PauseCanvas.SetActive(false);
+
+    }
+}
+
