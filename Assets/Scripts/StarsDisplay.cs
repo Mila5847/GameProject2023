9477f3f29e0c8dda5b3830b315bfd8629a388afa
Mila5847 <46633364+Mila5847@users.noreply.github.com>
2023-05-11 14:26:28 -0400

    star system for main menu done

--- Assets/Scripts/StarsDisplay.cs
+++ Assets/Scripts/StarsDisplay.cs
@@ -2,27 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StarsDisplay : MonoBehaviour
 {
-
-    public Text scoreText1;
+    public Transform starContainer1; // The container for the stars
+    public Transform starContainer2; // The container for the stars
+    public Transform starContainer3; // The container for the stars
+    public GameObject starPrefab; // The prefab for the stars
 
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get the current score from wherever it's stored
-        int newScore = Constants.totalPointsLevel1;
 
-       // Update the score text
-       scoreText1.text = newScore.ToString();
-        Debug.Log(Constants.totalPointsLevel1);
-    }   
+      
+            Debug.Log("IN IT");
+            // Get the current score from wherever it's stored
+            int newScore1 = Constants.totalPointsLevel1;
+
+            // Destroy any previous stars in the container
+            foreach (Transform child in starContainer1)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // Instantiate new stars in the container based on the score
+            for (int i = 0; i < newScore1; i++)
+            {
+                GameObject star = Instantiate(starPrefab, starContainer1);
+                star.transform.localPosition = new Vector3(i * 100f, 0f, 0f);
+            }
+      
+            // Get the current score from wherever it's stored
+            int newScore2 = Constants.totalPointsLevel2;
+
+            // Destroy any previous stars in the container
+            foreach (Transform child in starContainer2)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // Instantiate new stars in the container based on the score
+            for (int i = 0; i < newScore2; i++)
+            {
+                GameObject star = Instantiate(starPrefab, starContainer2);
+                star.transform.localPosition = new Vector3(i * 100f, 0f, 0f);
+            }
+     
+            // Get the current score from wherever it's stored
+            int newScore3 = Constants.totalPointsLevel3;
+
+            // Destroy any previous stars in the container
+            foreach (Transform child in starContainer3)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // Instantiate new stars in the container based on the score
+            for (int i = 0; i < newScore3; i++)
+            {
+                GameObject star = Instantiate(starPrefab, starContainer3);
+                star.transform.localPosition = new Vector3(i * 100f, 0f, 0f);
+            }
+      
+    }
 }
